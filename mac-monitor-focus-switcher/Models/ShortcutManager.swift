0422922f9a43f5f7c9d8163c4af4ed744be3e927
Mik-kom4 <androidmikiya9814@gmail.com>
2024-07-31 23:05:39 +0900

    add: ShortcutManagerクラス

--- mac-monitor-focus-switcher/Models/ShortcutManager.swift
+++ mac-monitor-focus-switcher/Models/ShortcutManager.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class ShortcutManager: ObservableObject {
+    @Published var shortcuts: [Shortcut] = []
+    
+    func addShortcut(KeyCombination: String, coordinates: (x: Double, y: Double)) {
+        let shortcut = Shortcut(keyCombination: KeyCombination, coordinates: coordinates)
+        shortcuts.append(shortcut)
+    }
+    
+    func getCoordinates(for keyCombination: String) -> (x: Double, y: Double)? {
+        return shortcuts.first { $0.keyCombination == keyCombination }?.coordinates
+    }
+}
