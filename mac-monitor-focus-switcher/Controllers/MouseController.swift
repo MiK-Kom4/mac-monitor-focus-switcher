69bea96204c774329a3e3cadbc6cf6a63043be76
Mik-kom4 <androidmikiya9814@gmail.com>
2024-07-31 22:51:08 +0900

    add: MouseControllerクラス

--- mac-monitor-focus-switcher/Controllers/MouseController.swift
+++ mac-monitor-focus-switcher/Controllers/MouseController.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import AppKit
+
+class MouseController {
+    func moveMouse(to coordinates: (x: Double, y: Double)) {
+        let mouseLocation = CGPoint(x: coordinates.x, y: coordinates.y)
+        CGWarpMouseCursorPosition(mouseLocation)
+    }
+}
