3bb13ea3ef0bc334e5e9693de82e15e671c49139
Mik-kom4 <androidmikiya9814@gmail.com>
2024-07-31 23:42:03 +0900

    add:ShortcutViewクラス 新しいショートカットを登録するためのビュー ユーザーがキーの組み合わせとマウスの座標を入力しShortcutManagerに追加

--- mac-monitor-focus-switcher/Views/ShortcutView.swift
+++ mac-monitor-focus-switcher/Views/ShortcutView.swift
@@ -5,4 +5,44 @@
 //  Created by 小松幹弥 on 2024/07/31.
 //
 
-import Foundation
+import SwiftUI
+
+struct ShortcutView: View {
+    @ObservedObject var shortcutManager: ShortcutManager
+    @State private var keyCombination = ""
+    @State private var xCoordinate = ""
+    @State private var yCoordinate = ""
+    
+    var body: some View {
+        VStack {
+            TextField("Key Combination", text: $keyCombination)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            
+            TextField("X Coordinate", text: $xCoordinate)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            TextField("Y Coordinate", text: $yCoordinate)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            Button(action: {
+                if let x = Double(xCoordinate), let y = Double(yCoordinate) {
+                    shortcutManager.addShortcut(KeyCombination: keyCombination, coordinates: (x, y))
+                }
+            }) {
+                Text("Add Shortcut")
+            }
+            .padding()
+        }
+        .navigationTitle("Add Shortcut")
+    }
+}
+
+struct ShortcutView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShortcutView(shortcutManager: ShortcutManager())
+    }
+}
